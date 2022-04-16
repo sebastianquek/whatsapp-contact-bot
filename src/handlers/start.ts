9b044d5b69932390aeb5a0ae1827c3ba5451988d
Sebastian Quek <sebq13@gmail.com>
2022-04-16 14:00:53 +0800

    Revert "feat: add numeric keyboard"
    This reverts commit fdc632ddfbd333e125dc58395fc0463fa15524e3.

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -4,11 +4,6 @@ export const handleStart = async (ctx: Context) => {
   return ctx.reply(
     `👋 Hello ${
       ctx.from?.first_name ?? "there"
-    }, enter a phone number you'd like to start a WhatsApp chat with.`,
-    {
-      reply_markup: {
-        keyboard: [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["0"]],
-      },
-    }
+    }, enter a phone number you'd like to start a WhatsApp chat with.`
   );
 };
